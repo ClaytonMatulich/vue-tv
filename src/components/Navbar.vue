482abee062ccfb1aa064a1f41e698bd6debcb674
Clayton Matulich <claytonmatulich13@gmail.com>
2019-08-05 16:37:51 -0700

    Gradient styling, sorting ui improvements

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,11 @@
 <template>
   <nav>
     <v-app-bar dark app flat>
-      <v-app-bar-nav-icon @click="drawer = !drawer" class="cyan--text text--lighten-3"></v-app-bar-nav-icon>
+      <v-app-bar-nav-icon
+        @click="drawer = !drawer"
+        class="cyan--text text--lighten-3"
+        style="background-color: red;"
+      ></v-app-bar-nav-icon>
       <v-toolbar-title class="text-uppercase grey--text">
         <span class="font-weight-light">vue</span>
         <span>TV</span>
@@ -9,8 +13,6 @@
     </v-app-bar>
 
     <v-navigation-drawer app v-model="drawer">
-      <v-layout column align-center></v-layout>
-
       <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
         <v-list-item-action>
           <v-icon class="white--text">{{link.icon}}</v-icon>
@@ -38,4 +40,7 @@
 </script>
 
 <style>
+  .v-navigation-drawer {
+    background: linear-gradient(251deg, #1f6b78, #876bdf);
+  }
 </style>
--- src/views/NowPlaying.vue
+++ src/views/NowPlaying.vue
@@ -1,76 +1,49 @@
 <template>
-  <div>
-    <v-container class="my-5">
-      <v-layout justify-end row class="mt-3 mr-4">
-        <v-flex class="ml-3">
-          <h1 class="display-1 font-weight-light">Now Playing</h1>
-        </v-flex>
-        <v-spacer></v-spacer>
-        <v-tooltip top>
-          <template v-slot:activator="{on}">
-            <v-btn
-              v-on="on"
-              small
-              outlined
-              color="cyan lighten-3"
-              class="mr-3"
-              @click="sortBy('popularity')"
-            >
-              <v-icon left>mdi-account-heart</v-icon>
-              <span class="caption text-lowercase">By popularity</span>
-            </v-btn>
-          </template>
-          <span>Sort movies by popularity</span>
-        </v-tooltip>
+  <v-container class="my-5">
+    <v-layout justify-end row class="mt-3 mr-4">
+      <v-flex class="ml-5">
+        <h1 class="display-1 font-weight-light">Now Playing</h1>
+        <h5 class="font-weight-light">{{sortCriteria}}</h5>
+      </v-flex>
+      <v-spacer></v-spacer>
+      <v-tooltip top>
+        <template v-slot:activator="{on}">
+          <v-btn v-on="on" small color="cyan lighten-3" class="mr-3" @click="sortBy('popularity')">
+            <v-icon left>mdi-account-heart</v-icon>
+            <span class="caption text-lowercase">By popularity</span>
+          </v-btn>
+        </template>
+        <span>Sort movies by popularity</span>
+      </v-tooltip>
 
-        <v-tooltip top>
-          <template v-slot:activator="{on}">
-            <v-btn v-on="on" small outlined color="cyan lighten-3" @click="sortBy('vote_average')">
-              <v-icon left>mdi-star</v-icon>
-              <span class="caption text-lowercase">By rating</span>
-            </v-btn>
-          </template>
-          <span>Sort movies by rating</span>
-        </v-tooltip>
-      </v-layout>
+      <v-tooltip top>
+        <template v-slot:activator="{on}">
+          <v-btn v-on="on" small color="cyan lighten-3" @click="sortBy('vote_average')">
+            <v-icon left>mdi-star</v-icon>
+            <span class="caption text-lowercase">By rating</span>
+          </v-btn>
+        </template>
+        <span>Sort movies by rating</span>
+      </v-tooltip>
+    </v-layout>
 
-      <v-layout row wrap>
-        <v-flex xs12 sm6 md4 lg3 v-for="movie in movies" :key="movie.id">
-          <v-card class="text-center ma-3" min-height="350" max-height="450">
-            <v-img :src="imageURL + movie.backdrop_path"></v-img>
-
-            <v-card-title class="font-weight-light">{{movie.title}}</v-card-title>
-
-            <v-divider class="mx-4"></v-divider>
-
-            <v-layout row>
-              <v-flex>
-                <v-card-text>
-                  <v-progress-circular
-                    size="70"
-                    color="cyan lighten-3"
-                    rotate="-90"
-                    width="8"
-                    :value="movie.vote_average * 10"
-                    class="font-weight-heavy"
-                  >{{movie.vote_average * 10}}</v-progress-circular>
-                </v-card-text>
-              </v-flex>
-            </v-layout>
-          </v-card>
-        </v-flex>
-      </v-layout>
-    </v-container>
-  </div>
+    <movie-grid :movies="movies" :imageURL="imageURL"></movie-grid>
+  </v-container>
 </template>
 
 <script>
   import axios from "axios";
+  import MovieGrid from "../components/MovieGrid.vue";
+
   export default {
+    components: {
+      movieGrid: MovieGrid
+    },
     data: function() {
       return {
         movies: [],
-        imageURL: "https://image.tmdb.org/t/p/w1280"
+        imageURL: "https://image.tmdb.org/t/p/w1280",
+        sortCriteria: "Most Popular"
       };
     },
     methods: {
@@ -97,6 +70,12 @@
           });
       },
       sortBy(prop) {
+        if (prop === "popularity") {
+          this.sortCriteria = "Most Popular";
+        } else if (prop === "vote_average") {
+          this.sortCriteria = "Highest Rated";
+        }
+
         this.movies.sort((a, b) => (a[prop] > b[prop] ? -1 : 1));
       }
     },
@@ -107,4 +86,7 @@
 </script>
 
 <style>
+  .v-btn {
+    background: linear-gradient(251deg, #1f6b78, #876bdf);
+  }
 </style>
