34ab928d4f012d718584178535e136d439aa8866
Clayton Matulich <claytonmatulich13@gmail.com>
2019-08-05 13:52:07 -0700

    movie sorting feature

--- src/views/NowPlaying.vue
+++ src/views/NowPlaying.vue
@@ -1,24 +1,62 @@
 <template>
   <div>
     <v-container class="my-5">
-      <h1 class="display-1">Now Playing</h1>
+      <v-layout justify-end row class="mt-3 mr-4">
+        <v-flex class="ml-3">
+          <h1 class="display-1 font-weight-light">Now Playing</h1>
+        </v-flex>
+        <v-spacer></v-spacer>
+        <v-tooltip top>
+          <template v-slot:activator="{on}">
+            <v-btn
+              v-on="on"
+              small
+              outlined
+              color="cyan lighten-3"
+              class="mr-3"
+              @click="sortBy('popularity')"
+            >
+              <v-icon left>mdi-account-heart</v-icon>
+              <span class="caption text-lowercase">By popularity</span>
+            </v-btn>
+          </template>
+          <span>Sort movies by popularity</span>
+        </v-tooltip>
+
+        <v-tooltip top>
+          <template v-slot:activator="{on}">
+            <v-btn v-on="on" small outlined color="cyan lighten-3" @click="sortBy('vote_average')">
+              <v-icon left>mdi-star</v-icon>
+              <span class="caption text-lowercase">By rating</span>
+            </v-btn>
+          </template>
+          <span>Sort movies by rating</span>
+        </v-tooltip>
+      </v-layout>
 
       <v-layout row wrap>
         <v-flex xs12 sm6 md4 lg3 v-for="movie in movies" :key="movie.id">
           <v-card class="text-center ma-3" min-height="350" max-height="450">
             <v-img :src="imageURL + movie.backdrop_path"></v-img>
-            <v-card-title>{{movie.title}}</v-card-title>
+
+            <v-card-title class="font-weight-light">{{movie.title}}</v-card-title>
 
             <v-divider class="mx-4"></v-divider>
-            <v-card-text>
-              <v-progress-circular
-                size="70"
-                color="cyan lighten-3"
-                rotate="-90"
-                width="12"
-                :value="movie.vote_average * 10"
-              >{{movie.vote_average * 10}}</v-progress-circular>
-            </v-card-text>
+
+            <v-layout row>
+              <v-flex>
+                <v-card-text>
+                  <v-progress-circular
+                    size="70"
+                    color="cyan lighten-3"
+                    rotate="-90"
+                    width="8"
+                    :value="movie.vote_average * 10"
+                    class="font-weight-heavy"
+                  >{{movie.vote_average * 10}}</v-progress-circular>
+                </v-card-text>
+              </v-flex>
+            </v-layout>
           </v-card>
         </v-flex>
       </v-layout>
@@ -57,6 +95,9 @@
           .finally(() => {
             // always executed
           });
+      },
+      sortBy(prop) {
+        this.movies.sort((a, b) => (a[prop] > b[prop] ? -1 : 1));
       }
     },
     mounted() {
