bc4f2568fd449f24c4e2c7bb239f7e99d5e7932a
Clayton Matulich <claytonmatulich13@gmail.com>
2019-08-06 16:05:58 -0700

    Pagination only shows up once content is loaded to reduce jitter look

--- src/views/TopRatedShows.vue
+++ src/views/TopRatedShows.vue
@@ -8,7 +8,7 @@
       @release_date="sortBy('release_date')"
     ></media-nav>
     <media-grid :movies="shows" :imageURL="imageURL"></media-grid>
-    <div class="text-center">
+    <div class="text-center" v-if="showPagination">
       <v-pagination color="indigo accent-2" v-model="page" :length="3" :value="page"></v-pagination>
     </div>
   </v-container>
@@ -31,7 +31,8 @@
         imageURL: "https://image.tmdb.org/t/p/w1280",
         sortCriteria: "Most Popular",
         sortedBy: "popularity",
-        page: 1
+        page: 1,
+        showPagination: false
       };
     },
     methods: {
@@ -56,6 +57,7 @@
           .finally(() => {
             // always executed
             this.sortBy(this.sortedBy);
+            this.showPagination = true;
           });
       },
       sortBy(prop) {
