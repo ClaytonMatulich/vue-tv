2a13aed3c5ce522f3a36198766a00d084f628d87
Clayton Matulich <claytonmatulich13@gmail.com>
2019-08-06 17:46:28 -0700

    Added buttons on landing page that link to the categorical pages

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,34 +1,56 @@
 <template>
   <div>
-    <v-container>
-      <v-layout column align-center justify-space-between>
-        <div class="text-center">
-          <h1
-            id="intro"
-            class="display-3 my-2 font-weight-thin"
-          >Discover Movies and Shows with VueTV</h1>
-        </div>
-
-        <v-img
-          contain
-          width="60vw"
-          height="60vh"
-          src="../assets/undraw_horror_movie.svg"
-          class="my-2"
-        ></v-img>
-      </v-layout>
-    </v-container>
+    <v-layout column align-center justify-space-between fill-height>
+      <div class="text-center">
+        <h1 id="intro" class="display-3 my-3 font-weight-thin">Discover Movies and Shows with VueTV</h1>
+      </div>
+      <v-spacer></v-spacer>
+      <v-img
+        contain
+        width="60vw"
+        height="60vh"
+        src="../assets/undraw_horror_movie.svg"
+        class="mt-2"
+      ></v-img>
+    </v-layout>
+    <v-layout align-center row class="my-5" wrap>
+      <v-flex xs12 sm6 md3 class="text-center" v-for="link in links" :key="link.location">
+        <v-btn min-width="200" class="mx-5 my-2" router :to="link.path">{{link.location}}</v-btn>
+      </v-flex>
+    </v-layout>
   </div>
 </template>
 
 <script>
   export default {
-    components: {}
+    components: {},
+    data() {
+      return {
+        links: [
+          {
+            location: "Movies Playing Now",
+            path: "/movies-playing-now"
+          },
+          {
+            location: "Top Rated Movies",
+            path: "/top-rated-movies"
+          },
+          {
+            location: "Popular Shows",
+            path: "/popular-shows"
+          },
+          {
+            location: "Top Rated Shows",
+            path: "/top-rated-shows"
+          }
+        ]
+      };
+    }
   };
 </script>
 
 <style scoped>
-  .h1 {
-    background-color: linear-gradient(251deg, #1f6b78, #876bdf);
+  .v-btn {
+    background: linear-gradient(251deg, #1f6b78, #876bdf);
   }
 </style>
