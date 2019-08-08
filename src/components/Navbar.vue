4e51716786859900521ad1c07af21f94012aaa84
Clayton Matulich <claytonmatulich13@gmail.com>
2019-08-07 17:20:28 -0700

    Added tv logo to nav bar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -9,6 +9,12 @@
     </v-app-bar>
 
     <v-navigation-drawer app v-model="drawer" color="indigo accent-2">
+      <v-layout column align-center>
+        <v-flex class="my-5">
+          <v-img max-width="100px" src="../assets/retro-tv.svg"></v-img>
+        </v-flex>
+      </v-layout>
+
       <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
         <v-list-item-action>
           <v-icon class="white--text">{{link.icon}}</v-icon>
